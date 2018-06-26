43f0dedde9383f111fdabbb15e082f99c35496b5
yash1337 <developeraccys@gmail.com>
2018-06-26 10:57:46 +0530

    created config file to setup enviornment variables

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 import os
-def load_env_variables():
+
+def load_env_variables(): #To load the config varibles before creating the flask object
     from dotenv import load_dotenv
     load_dotenv()
-
-class BaseConfig:
+    
+#To load cofig variables from object.
+class BaseConfig: 
     DEBUG=False
     TESTING=False
     SECRET_KEY=os.getenv("SECRET_KEY")
