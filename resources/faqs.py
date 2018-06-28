b5c81028e1549593e7ff60806a52f32a849b63c2
yash1337 <developeraccys@gmail.com>
2018-06-28 13:51:55 +0530

    initialized endpoint class

---
+++ resources/faqs.py
@@ -0,0 +1,15 @@
+from flask_restful import Resource
+
+class Faqs(Resource):
+
+    def post(self):
+        pass
+
+    def get(self,faq_id):
+        pass
+
+    def put(self,faq_id):
+        pass
+
+    def delete(self,faq_id):
+        pass
