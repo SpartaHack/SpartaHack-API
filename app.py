fb846841ab20f3d4b7d8b9483ab42507fa790fd0
yash1337 <developeraccys@gmail.com>
2018-07-03 13:21:55 +0530

    Added code fo doing db refelction before first request is encountered. Added meta-data about the API at root url.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
 from celery import Celery
 from flask_sqlalchemy import SQLAlchemy
@@ -15,12 +15,19 @@ app.config.from_object(DevelopmentConfig)#loading config data into flask app fro
 api = Api(app)
 
 db = SQLAlchemy(app)
-#db.reflect(app=app)#to reflect on the already defined database
+
+@app.before_first_request
+def initialize_app():
+    db.reflect(app=app)#to reflect on the already defined database
 
 task_queue=Celery("SpartaHack_API_2019",broker=app.config["CELERY_BROKER_URL"])
 
 api.add_resource(Faqs_RUD,"/faqs/<int:faq_id>")
 api.add_resource(Faqs_CR,"/faqs/")
+
+@app.route("/")#for flask app test and general info about the product
+def helloworld():
+    return jsonify({"Organisation":"SpartaHack","Backend Developers":"Yash, Jarek","Frontend Developers":"Harrison, Jessica, Jarek","Contact":"hello@spartahack.com"})
 
 if __name__ == '__main__': #running on local server. This needs to change for prod
     app.run(debug=True)
