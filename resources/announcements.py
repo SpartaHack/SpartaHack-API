2f5a7a33a2899d0249c9a3de724fddd90ff5a04d
yash1337 <developeraccys@gmail.com>
2018-07-15 18:32:55 +0530

    Added code for DELETE and POST request on the announcement endpoint.

--- resources/announcements.py
+++ resources/announcements.py
@@ -55,13 +55,32 @@ class Announcements_RUD(Resource):
                 return (not_found,404,headers)
 
     def delete(self,announcement_id):
-        pass
+        """
+        For DELETE request for specific id
+        """
+        user_status,user = has_admin_privileges()
+        if user_status == "no_auth_token":
+            return (bad_request,400,headers)
 
+        if user_status == "not_logged_in":
+            return (unauthorised,401,headers)
+
+        if user_status == True:
+            try:
+                g.session.query(g.Base.classes.announcements).filter(g.Base.classes.announcements.id == announcement_id).delete()
+                return ("",204,headers)
+            except NoResultFound:
+                return (not_found,404,headers)
+        else:
+            return (forbidden,403,headers)
 class Announcements_CR(Resource):
     """
     For GET PUT and DELETE for specific announcement
     """
     def get(self):
+        """
+        Getting all the announcements by hitting /announcements endpoint without any specific id
+        """
         try:
             all_announcements = g.session.query(g.Base.classes.announcements).all()
             ret = Announcement_Schema(many = True).dump(all_announcements).data
@@ -70,4 +89,43 @@ class Announcements_CR(Resource):
             return (internal_server_error,500,headers)
 
     def post(self):
-        pass
+        """
+        For POST request. Checks for bad JSON from request, checks if data types in request JSON are correct, Checks user auth status and if he's logged in.
+        """
+        try:
+            data = request.get_json(force=True)
+        except BadRequest:
+            return (bad_request,400,headers)
+
+        #data validation
+        if Announcement_Schema().validate(data):
+            return (unprocessable_entity,422,headers)
+
+        #check if user has admin privileges
+        user_status,user = has_admin_privileges()
+        if user_status == "no_auth_token":
+            return (bad_request,400,headers)
+
+        if user_status == "not_logged_in":
+            return (unauthorised,401,headers)
+
+        if user_status == True:
+            Announcements = g.Base.classes.announcements
+            try:
+                new_announcement = Announcements(
+                                                    title = data["title"],
+                                                    description = data["description"],
+                                                    pinned = data["pinned"],
+                                                    updated_at = datetime.now(),
+                                                    created_at = datetime.now()
+                                                )
+                g.session.add(new_announcement)
+                g.session.commit()
+                ret = g.session.query(g.Base.classes.announcements).filter(g.Base.classes.announcements.description == data["description"]).one()
+                ret = Announcement_Schema().dump(ret).data
+                return (ret,201 ,headers)
+            except Exception as err:
+                print(type(err))
+                print(err.args)
+                print(err)
+                return (internal_server_error,500,headers)
