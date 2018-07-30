174129ac3f6e6da1510fac3c63d4d8106c6fc356
yash1337 <developeraccys@gmail.com>
2018-07-30 15:38:50 +0530

    added code for DELETE and POST

--- resources/sponsors.py
+++ resources/sponsors.py
@@ -31,15 +31,89 @@ class Sponsor_RUD(Resource):
         else:
             return (not_found,404,headers)
 
-    def put(self,sponsor_id):
-        pass
-
     def delete(self,sponsor_id):
-        pass
+        user_status,user = has_admin_privileges()
+        if user_status == "no_auth_token":
+            return (bad_request,400,headers)
+
+        if user_status == "not_logged_in":
+            return (z,401,headers)
+
+        if user_status in ["director","organizer"]:
+            try:
+                #this makes sure that at least one hardware matches hardware id
+                sponsor_to_delete = g.session.query(g.Base.classes.sponsors).get(sponsor_id)
+                if sponsor_to_delete:
+                    g.session.query(g.Base.classes.sponsors).filter(g.Base.classes.sponsors.id == sponsor_id).delete()
+                    return ("",204,headers)
+                else:
+                    return (not_found,404,headers)
+            except Exception as err:
+                print(type(err))
+                print(err)
+                return (internal_server_error,500,headers)
+        else:
+            return (forbidden,403,headers)
 
 class Sponsor_CR(Resource):
     def post(self):
-        pass
+        """
+        For POST request. Checks for bad JSON from request, checks if data types in request JSON are correct, Checks user auth status and if he's logged in.
+        """
+        try:
+            data = request.get_json(force=True)
+        except BadRequest:
+            return (bad_request,400,headers)
+
+        #data validation
+        if Sponsor_Schema().validate(data):
+            return (unprocessable_entity,422,headers)
+
+        #check if user has admin privileges
+        user_status,user = has_admin_privileges()
+        if user_status == "no_auth_token":
+            return (bad_request,400,headers)
+
+        if user_status == "not_logged_in":
+            return (unauthorized,401,headers)
+
+        #checking if announcement with same title and description already exists. To manage duplicate entries
+        try:
+            exist_check = g.session.query(exists().where(and_(g.Base.classes.sponsors.name == data["name"],g.Base.classes.sponsors.url == data["url"]))).scalar()
+            if exist_check:
+                return (conflict,409,headers)
+        except Exception as err:
+            print(type(err))
+            print(err)
+            return (internal_server_error,500,headers)
+
+        if user_status in ["director","organizer"]:
+            Sponsors = g.Base.classes.announcements
+            try:
+                new_sponsor = Sponsors(
+                                        name = data["name"],
+                                        url = data["url"],
+                                        level = data["level"],
+                                        logo_svg_light = data["logo_svg_light"],
+                                        logo_png_light = data["logo_png_light"],
+                                        updated_at = datetime.now(),
+                                        created_at = datetime.now()
+                                      )
+                if data["logo_svg_dark"]:
+                    new_sponsor.logo_svg_dark = data["logo_svg_dark"]
+                if data["logo_png_dark"]:
+                    new_sponsor.logo_png_dark = data["logo_png_dark"]
+
+                g.session.add(new_sponsor)
+                g.session.commit()
+                ret = g.session.query(g.Base.classes.sponsors).filter(g.Base.classes.sponsors.name == data["name"]).one()
+                return (Sponsor_Schema().dump(ret).data,201,headers)
+            except Exception as err:
+                print(type(err))
+                print(err)
+                return (internal_server_error,500,headers)
+        else:
+            return(forbidden,403,headers)
 
     def get(self):
         """
